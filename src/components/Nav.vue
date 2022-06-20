056ab4f4bdb2f82824efcfa9e7b98de30593f435
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-06-20 14:39:54 +0530

    nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,14 @@
 <template>
-  Nav
+  <div class="nav-bar">
+    <div class="title">
+      BasicAdmin
+    </div>
+    <ul class="top-nav">
+      <li>
+        <a href="/">Logout</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
