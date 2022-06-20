812608638bb02ceb4a66a1250aa877ac150909d0
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-06-20 20:27:56 +0530

    Products

--- src/pages/admin/Products.vue
+++ src/pages/admin/Products.vue
@@ -1,11 +1,13 @@
 <template>
  <table>
-    <tr>
+   <thead>
+      <tr>
         <th>#id</th>
         <th>Title</th>
         <th>Image</th>
         <th>Action</th>
     </tr>
+   </thead>
     <tbody>
     </tbody>
  </table>
