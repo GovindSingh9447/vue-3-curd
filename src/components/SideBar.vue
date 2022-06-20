8ead1463f621348ca1bdb005f79d0f960b7cbdfe
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-06-20 14:55:22 +0530

    db

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,5 +1,10 @@
 <template>
-  Side nav
+   <div class="siderbar">
+    <ul class="side-menu">
+      <li><router-link :to="{name: 'Products'}">Products</router-link></li>
+      <li><router-link :to="{name :'ProductCreate'}">Add Product</router-link></li>
+    </ul>
+   </div>
 </template>
 
 <script>
